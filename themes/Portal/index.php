b410a640af162387f33ae561696b9a85c9def66c
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2013-05-22 03:09:57 +0300

    Blogs module and profile settings fixes. Theme changes.

--- themes/Portal/index.php
+++ themes/Portal/index.php
@@ -1,24 +1,12 @@
 <!doctype html>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <!--pre_Html--><!--head--><!--pre_Body--><!--debug_info-->
-<header>
-<!--header-->
-	<nav class="cs-header-main-menu">
-<!--main-menu-->
-	</nav>
-	<nav class="cs-header-main-submenu">
-<!--main-submenu-->
-	</nav>
-	<nav class="cs-header-menu-more">
-<!--menu-more-->
-	</nav>
-	<div class="cs-header-user">
-		<div class="cs-header-avatar" style="background-image:<!--user_avatar_image-->;"></div>
-		<div class="cs-header-user-info">
+<div class="cs-header-user">
+	<div class="cs-header-avatar" style="background-image:<!--user_avatar_image-->;"></div>
+	<div class="cs-header-user-info">
 <!--header_info-->
-		</div>
 	</div>
-</header>
+</div>
 <div id="cs-body">
 <?php
 global $Page;
@@ -33,19 +21,14 @@ if ($Page->Left) {
 		'code'
 	);
 }
-if ($Page->Right) {
 ?>
 	<aside id="cs-right-blocks">
+		<nav class="cs-header-main-menu">
+			<!--main-menu-->
+		</nav>
 <!--right_blocks-->
 	</aside>
-<?php
-} else {
-	$Page->css(
-		'#cs-top-blocks, #cs-bottom-blocks, #cs-main-content {margin-right:0}',
-		'code'
-	);
-}
-?>	<aside id="cs-top-blocks">
+	<aside id="cs-top-blocks">
 <!--top_blocks-->
 	</aside>
 	<div id="cs-main-content">
