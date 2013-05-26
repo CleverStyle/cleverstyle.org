7c1c58acd5118eac15edb83c749e6520a04546a5
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2013-05-27 00:35:45 +0300

    CMS page and theme changes.

---
+++ components/modules/CMS/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package		Home
+ * @category	modules
+ * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright	Copyright (c) 2011-2013, Nazar Mokrynskyi
+ * @license		MIT License, see license.txt
+ */
+global $Index, $L;
+$Index->content(file_get_contents(__DIR__.'/index_'.$L->clang.'.html'));
