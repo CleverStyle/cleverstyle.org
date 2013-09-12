b90cee0918b0fe015033749eaff3dee868450afb
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2013-09-12 22:53:23 +0300

    System updated to latest version.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		CleverStyle CMS
- * @version		0.325
+ * @version		0.327
  * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
  * @copyright	Copyright (c) 2011-2013, Nazar Mokrynskyi
  * @license		MIT License, see license.txt
