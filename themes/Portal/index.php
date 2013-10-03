44fbaa3c345dc9635ce9058ea32f69b318032818
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2013-10-03 22:49:59 +0300

    HTML optimization.

--- themes/Portal/index.php
+++ themes/Portal/index.php
@@ -1,31 +1,35 @@
 <!doctype html>
-<!--pre_Html--><!--head--><!--pre_Body--><!--debug_info-->
-<aside id="cs-right-blocks">
-<div class="cs-header-user">
-	<div class="cs-header-avatar" style="background-image:url(<!--user-avatar-image-->);"></div>
+<!--pre_Html--><!--head-->
+<body>
+<!--pre_Body--><!--debug_info-->
+	<aside id="cs-right-blocks">
+	<div class="cs-header-user">
+		<div class="cs-header-avatar" style="background-image:url(<!--user-avatar-image-->);"></div>
 	<div class="cs-header-user-info">
 <!--header_info-->
+		</div>
 	</div>
-</div>
-	<nav class="cs-header-main-menu">
+		<nav class="cs-header-main-menu">
 <!--main-menu-->
-	</nav>
-	<div>
-	</div>
+		</nav>
+		<div>
+		</div>
 <!--right_blocks-->
-</aside>
-<div id="cs-body">
-	<aside id="cs-top-blocks">
+	</aside>
+	<div id="cs-body">
+		<aside id="cs-top-blocks">
 <!--top_blocks-->
-	</aside>
-	<div id="cs-main-content">
+		</aside>
+		<div id="cs-main-content">
 <!--content-->
-	</div>
-	<aside id="cs-bottom-blocks">
+		</div>
+		<aside id="cs-bottom-blocks">
 <!--bottom_blocks-->
-	</aside>
-</div>
-<footer>
+		</aside>
+	</div>
+	<footer>
 <!--footer-->
-</footer>
-<!--post_Body--><!--post_Html-->
+	</footer>
+<!--post_Body-->
+</body>
+<!--post_Html-->
