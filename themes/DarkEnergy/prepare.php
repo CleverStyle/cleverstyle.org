68925c4afed565240e35c7e8f825437d1d1fe657
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-04-08 03:28:27 +0300

    Theme DarkEnergy + Home page

--- themes/DarkEnergy/prepare.php
+++ themes/DarkEnergy/prepare.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * @package		ClevereStyle CMS
- * @subpackage	CleverStyle theme
+ * @subpackage	DarkEnergy theme
  * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2013-2014, Nazar Mokrynskyi
+ * @copyright	Copyright (c) 2014, Nazar Mokrynskyi
  * @license		MIT License, see license.txt
  */
 namespace	cs;
+$Page	= Page::instance();
 if (isset($_SERVER['HTTP_USER_AGENT']) && preg_match('/msie|trident/i',$_SERVER['HTTP_USER_AGENT'])) {
-	Page::instance()->Head	.= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+	$Page->Head	.= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
 }
+$Page->Head	.= '<meta name="viewport" content="width=device-width">';
