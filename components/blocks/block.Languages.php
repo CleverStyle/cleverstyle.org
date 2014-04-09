2b5f783a24f2114a14b5c1f10162f81b386c4bbd
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-04-09 20:54:30 +0300

    Simpler languages block

--- components/blocks/block.Languages.php
+++ components/blocks/block.Languages.php
@@ -7,10 +7,10 @@
  * @license		MIT License, see license.txt
  */
 namespace	cs;
-$Config	= Config::instance();
+$address	= MODULE == 'Home' ? '' : '/'.Config::instance()->server['relative_address'];
 ?>
 <nav>
-	<a href="<?='/en'.(MODULE == 'Home' ? '' : '/'.$Config->server['relative_address'])?>" hreflang="en">English</a>
-	<a href="<?='/ru'.(MODULE == 'Home' ? '' : '/'.$Config->server['relative_address'])?>" hreflang="ru">Русский</a>
-	<a href="<?='/uk'.(MODULE == 'Home' ? '' : '/'.$Config->server['relative_address'])?>" hreflang="uk">Українська</a>
+	<a href="/en<?=$address?>" hreflang="en">English</a>
+	<a href="/ru<?=$address?>" hreflang="ru">Русский</a>
+	<a href="/uk<?=$address?>" hreflang="uk">Українська</a>
 </nav>
