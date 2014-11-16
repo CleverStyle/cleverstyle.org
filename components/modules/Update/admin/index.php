b3234942cf058d664aa3507ff0e17638a7625404
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-11-16 23:40:33 +0100

    Fix for Update module

--- components/modules/Update/admin/index.php
+++ components/modules/Update/admin/index.php
@@ -17,17 +17,17 @@ if (!$module_object->version) {
 }
 if (isset($Config->route[0]) && $Config->route[0] == 'process') {
 	$version		= (int)$module_object->version;
-	for ($i = 1; file_exists(MFOLDER."/sql/$i.sql") || file_exists(MFOLDER."/php/$i.php"); ++$i) {
+	for ($i = 1; file_exists(__DIR__."/sql/$i.sql") || file_exists(__DIR__."/php/$i.php"); ++$i) {
 		if ($version < $i) {
-			if (file_exists(MFOLDER."/sql/$i.sql")) {
-				foreach (explode(';', file_get_contents(MFOLDER."/sql/$i.sql")) as $s) {
+			if (file_exists(__DIR__."/sql/$i.sql")) {
+				foreach (explode(';', file_get_contents(__DIR__."/sql/$i.sql")) as $s) {
 					if ($s) {
 						$db->{'0'}()->q($s);
 					}
 				}
 			}
-			if (file_exists(MFOLDER."/php/$i.php")) {
-				include MFOLDER."/php/$i.php";
+			if (file_exists(__DIR__."/php/$i.php")) {
+				include __DIR__."/php/$i.php";
 			}
 			$module_object->version	= $i;
 		}
@@ -37,7 +37,7 @@ if (isset($Config->route[0]) && $Config->route[0] == 'process') {
 $Index->buttons	= false;
 $Index->content(
 	h::{'p.cs-center'}("Current revision: $module_object->version").
-	h::{'a.cs-button.cs-center'}(
+	h::{'a.uk-button.cs-center'}(
 		'Update System structure',
 		[
 			'href'	=> 'admin/Update/process'
