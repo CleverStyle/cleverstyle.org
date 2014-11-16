5bdfe36c63fdb585385baf2c74b4fb0c726c3f62
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-11-16 22:49:35 +0100

    Cron module updated ot latest version

--- components/modules/Cron/admin/prepare.php
+++ components/modules/Cron/admin/prepare.php
@@ -3,15 +3,11 @@
  * @package		Cron
  * @category	modules
  * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2011-2013, Nazar Mokrynskyi
+ * @copyright	Copyright (c) 2011-2014, Nazar Mokrynskyi
  * @license		MIT License, see license.txt
  */
 namespace	cs;
-use			h;
-if (
-	isset($_POST['edit_settings'], $_POST['tasks']) &&
-	$_POST['edit_settings'] == 'save'
-) {
+if (isset($_POST['save'], $_POST['tasks'])) {
 	$filename	= TEMP.'/'.uniqid('cron');
 	$tasks		= _trim(explode("\n", trim($_POST['tasks'])));
 	$tasks		= implode("\n", $tasks);
@@ -20,9 +16,3 @@ if (
 	unlink($filename);
 	Index::instance()->save($result === 0);
 }
-Page::instance()->main_sub_menu		= h::{'li.uk-active a'}(
-	'Crontab',
-	[
-		'href'	=> 'admin/Cron'
-	]
-);
