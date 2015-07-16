1a27e87f27870a249390e8849edf82a5d9ffc5a0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-07-16 05:07:26 +0200

    Update module updated to work nicely with latest version of system core

--- components/modules/Update/admin/index.php
+++ components/modules/Update/admin/index.php
@@ -1,46 +1,49 @@
 <?php
 /**
- * @package        Update
- * @category       modules
- * @author         Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright      Copyright (c) 2013, Nazar Mokrynskyi
- * @license        MIT License, see license.txt
+ * @package   Update
+ * @category  modules
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2013, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs;
-use			h;
-$Config			= Config::instance();
-$db				= DB::instance();
-$Index			= Index::instance();
-$module_object	= $Config->module('Update');
+namespace cs;
+use
+	h;
+$Config        = Config::instance();
+$Route         = Route::instance();
+$cdb           = DB::instance()->db_prime(0);
+$Index         = Index::instance();
+$module_object = $Config->module('Update');
 if (!$module_object->version) {
-	$module_object->version	= 0;
+	$module_object->version = 0;
 }
-if (isset($Config->route[0]) && $Config->route[0] == 'process') {
-	$version		= (int)$module_object->version;
+if (@$Route->route[0] == 'process') {
+	$version = (int)$module_object->version;
+	/** @noinspection CallableInLoopTerminationConditionInspection */
 	for ($i = 1; file_exists(__DIR__."/sql/$i.sql") || file_exists(__DIR__."/php/$i.php"); ++$i) {
 		if ($version < $i) {
 			if (file_exists(__DIR__."/sql/$i.sql")) {
 				foreach (explode(';', file_get_contents(__DIR__."/sql/$i.sql")) as $s) {
 					if ($s) {
-						$db->{'0'}()->q($s);
+						$cdb->q($s);
 					}
 				}
 			}
 			if (file_exists(__DIR__."/php/$i.php")) {
 				include __DIR__."/php/$i.php";
 			}
-			$module_object->version	= $i;
+			$module_object->version = $i;
 		}
 	}
 	$Index->save(true);
 }
-$Index->buttons	= false;
+$Index->buttons = false;
 $Index->content(
 	h::{'p.cs-center'}("Current revision: $module_object->version").
 	h::{'a.uk-button.cs-center'}(
 		'Update System structure',
 		[
-			'href'	=> 'admin/Update/process'
+			'href' => 'admin/Update/process'
 		]
 	)
 );
