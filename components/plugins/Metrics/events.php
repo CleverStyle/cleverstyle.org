ede38c2877e8a5b13645457867f07201a0def9cf
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2016-05-30 11:17:19 +0300

    Do not include Yandex.Metrics on page for those visitors who do not want to be tracked

--- components/plugins/Metrics/events.php
+++ components/plugins/Metrics/events.php
@@ -1,18 +1,23 @@
 <?php
 /**
- * @package        Metrics
- * @category       plugins
- * @author         Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright      Copyright (c) 2011-2013, Nazar Mokrynskyi
- * @license        MIT License, see license.txt
+ * @package   Metrics
+ * @category  plugins
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2011-2016, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs;
+namespace cs;
 Event::instance()->on(
 	'System/Page/render/before',
 	function () {
-		if (User::instance()->admin() || !in_array('Metrics', Config::instance()->components['plugins'])) {
+		if (
+			User::instance()->admin() ||
+			Request::instance()->header('dnt') == '1' ||
+			!in_array('Metrics', Config::instance()->components['plugins'])
+		) {
 			return;
 		}
-		Page::instance()->post_Body	.= '<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter1250519 = new Ya.Metrika({id:1250519, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/1250519" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->';
+		Page::instance()->post_Body .=
+			'<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter1250519 = new Ya.Metrika({id:1250519, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/1250519" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->';
 	}
 );
